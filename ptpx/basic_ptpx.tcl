a5ccb3388ed1e588f4ceb3b2bedb26149e84553e
ZEN872 <ivanmagana10@hotmail.com>
2025-06-01 10:38:46 -0700

    basic ptpx

---
+++ ptpx/basic_ptpx.tcl
@@ -0,0 +1,24 @@
+# Enable power analysis in PrimeTime
+set power_enable_analysis TRUE
+
+set target_library "/usr/local/synopsys/pdk/SAED32_EDK/lib/stdcell_rvt/db_ccs/saed32rvt_tt0p78v25c.db"
+set link_library [list {*} "/usr/local/synopsys/pdk/SAED32_EDK/lib/stdcell_rvt/db_ccs/saed32rvt_tt0p78v25c.db"]
+
+read_db $target_library
+
+# Read GCD netlist
+read_verilog "../syn/gcd_toplevel_synthesized.v"
+
+# Set top-level design
+current_design gcd_toplevel
+
+# Create 500 MHz clock
+create_clock -period 2 -name clk [find port clk]
+
+# Load VCD for switching activity
+read_vcd -strip_path gcd_tb/dut "../sim/gcd.vcd"
+
+# Save power reports
+report_power -nosplit -verbose > total_power.log
+report_power -cell -verbose > cell_power.log
+report_switching_activity -list_not_annotated > unannotated.log
